17bcb3f96a731e69858ca99b1777b5ea25711b3a
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-29 19:30:21 -0300

    Primeiro App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css"; // importei pra dentro do meu projeto TODO o css do arquivo app.css
 
 function App() {
+  const [cep, setCep] = useState("");
+  const [retorno, setRetorno] = useState({});
+
+  // criei um função assincrona para buscar o cep
+  async function buscaCEP() {
+    // const resultado aguarda e recebe o resultado da requisição (solução da promessa)
+    const resultado = await fetch("https://viacep.com.br/ws/01001000/json/");
+    // // const json aguarda e recebe o resultado da requisição (solução da promessa)
+    const json = await resultado.json();
+
+    setRetorno(json);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <input
+        type="text"
+        name="cep"
+        id="cep"
+        value={cep}
+        onChange={(e) => setCep(e.target.value)}
+      />
+      <div>
+        <p>CEP: </p>
+        <p>Logradouro: {retorno.logradouro}</p>
+        <p>Bairro: {retorno.bairro}</p>
+        <p>Cidade: {retorno.localidade}</p>
+        <p>Estado:{retorno.localidade}</p>
+      </div>
+      <button
+        onClick={() => {
+          buscaCEP();
+        }}
+      >
+        Busca CEP
+      </button>
     </div>
   );
 }
